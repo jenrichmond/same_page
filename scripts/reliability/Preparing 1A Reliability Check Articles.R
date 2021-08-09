45d90e64ce3c8d2dd4d52655cc985474ddd3a491
ChristinaGitHub1 <c.rochios@student.unsw.edu.au>
2021-08-09 17:29:50 +1000

    Christina running 1A reliability analysis

--- scripts/reliability/Preparing 1A Reliability Check Articles.R
+++ scripts/reliability/Preparing 1A Reliability Check Articles.R
@@ -4,45 +4,36 @@ library(qualtRics)
 library(tidyverse)
 library(janitor)
 
-# Import 1A data from qualtrics 
+# Import 1A reliability data from qualtrics 
 
-qualtrics_api_credentials(api_key = "RZNCuPM7MvUJmhi7wcSLN7NHx0WCVeIU6H7XiSeE", 
+qualtrics_api_credentials(api_key = "x6zwQgKWTkduv783BnlSXfwam8yGGYLRpEcZrAnF", 
                           base_url = "unsw.syd1.qualtrics.com",
                           install = TRUE, 
                           overwrite = TRUE)
 
 readRenviron("~/.Renviron")  
 
-# pull 1A data
-data1A  <- fetch_survey(surveyID = "SV_0GSSTcRvY9x0x4q", verbose = TRUE,force_request = TRUE) %>%
+reliability1A  <- fetch_survey(surveyID = "SV_1NVNjqwtoR2vfV4", verbose = TRUE,force_request = TRUE) %>%
   clean_names()
 
-# write 1A data to csv
-
-data1A %>% write_csv(here::here("data_files", "2021_07_22-data1A.csv"))
-
-#filter data1A to only include 'Check' articles and select just variables of interest
-data1A_select <- data1A %>%
-  filter(str_detect(q4_1, 'Check')) %>%
+#filter to select only variables of interest
+select1A <- reliability1A  %>%
   select(q2:q10_8_text)
 
 # let's rename the variables
-data1A_rename <- data1A_select %>%
+rename1A <- select1A %>%
   rename(coder_name = q2, coder_email = q3, no_of_experiments = q31, type_of_article = q33,         type_of_article_other = q33_7_text, participants = q5, 
          participants_other = q5_3_text, age = q6, brain_beh = q7, topic = q8, 
          topic_other = q8_5_text, software = q9, type_of_software = q10,       type_of_software_other = q10_8_text) 
 
 # separate article id number and journal code
-data1A_sep <- data1A_rename %>%
-  separate(q4_1, into = c("type_of_article", "check_id"), sep = "\\s", remove = FALSE) %>%
-  rename(article_id_number_and_title = q4_1)
-
-# remove any duplicated rows
-data1A_clean <- data1A_sep %>%
-  distinct()
+sep1A <- rename1A %>%
+  separate(q4_1, into = c("article_id_number", "check_id"), sep = "\\s", remove = FALSE) %>%
+  rename(article_id_number_and_title = q4_1) %>%
+  mutate(article_id_number = str_remove_all(article_id_number, "\""))
 
 # write a .csv file that only contains Reliability Check articles
-data1A_clean %>% write_csv(here::here("data_files", "data1A_reliability_checking"))
+sep1A %>% write_csv(here::here("data_files", "data1A_reliability_checking"))
 
 
 
--- scripts/reliability/Reliability Checking 1A.R
+++ scripts/reliability/Reliability Checking 1A.R
@@ -1,23 +1,44 @@
 # Load packages
-library(qualtRics)
 library(tidyverse)
 library(janitor)
-library(ggplot2)
-library(grid)
-library(cowplot)
-library(httr)
-library(extrafont)
 library(here)
 library(Hmisc)
 library("vcd")
 library(irr)
+library(psych)
 
-# Read in Reliability Checking data
+# GOLD STANDARD -----
 
-data <- read_csv(here("data_files", "data1A_reliability_checking")) %>%
-  select(coder_name, article_id_number_and_title, id_number = check_id, participants:topic_other)
+# Read in Christina's Reliability Checking data - gold standard
+
+gold_standard_data <- read_csv(here("data_files", "data1A_reliability_checking")) %>%
+  select(coder_name, article_id_number, participants:topic_other)
 
 # Let's assign the articles to a subfield
+
+gold_psyc_subfield <- gold_standard_data %>%
+  mutate(subfield = case_when("Animals" == participants ~ "Behavioural Neuroscience", 
+                              "Humans" == participants & "0-18 years or 65 years+" == age ~ "Developmental Psychology", 
+                              "Humans" == participants & "Brain" == brain_beh  ~ "Cognitive Neuroscience",
+                              "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Sensation" == topic ~ "Perception",
+                              "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Emotion, personality, social behaviour" == topic ~ "Social Psychology",
+                              "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Intelligence, memory, decision making, reasoning, language, problem solving, creative thinking" == topic ~ "Cognition",
+                              "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Fitness, weight, consumption, hormone levels, chemical uptake, sleeping patterns" == topic ~ "Health Psychology")) %>%
+  relocate(subfield, .after = article_id_number)
+
+# let's make the data long
+
+gold_subfield_long <- gold_psyc_subfield %>%
+  select(coder_name, article_id_number, subfield) %>%
+  pivot_wider(names_from = coder_name, values_from = subfield) 
+
+# CODER'S DATA -----
+
+# let's load in coder's reliability checking data
+
+data <- read_csv(here("data_files", "scored_master_dataset_1A.csv")) %>%
+  select(coder_name, article_id_number, participants:topic_other) %>%
+  filter(article_id_number %in% c("10-4-2014", "15-4-2014", "18-4-2015", "6-1-2015", "9-4-2014", "2-6-2014", "2-7-2014", "22-4-2014", "4-2-2015", "5-2-2015", "1-10-2014", "12-9-2014", "5-10-2014", "5-3-2015", "7-1-2015", "1-3-2015", "10-3-2014", "14-8-2014", "8-5-2014", "9-9-2014", "24-2-2014", "5-3-2014", "5-5-2014", "5-6-2014", "7-3-2014"))
 
 psyc_subfield <- data %>%
   mutate(subfield = case_when("Animals" == participants ~ "Behavioural Neuroscience", 
@@ -27,129 +48,127 @@ psyc_subfield <- data %>%
                               "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Emotion, personality, social behaviour" == topic ~ "Social Psychology",
                               "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Intelligence, memory, decision making, reasoning, language, problem solving, creative thinking" == topic ~ "Cognition",
                               "Humans" == participants & brain_beh %in% c("Both", "Behaviour") & "Fitness, weight, consumption, hormone levels, chemical uptake, sleeping patterns" == topic ~ "Health Psychology")) %>%
-  relocate(subfield, .after = id_number)
+  relocate(subfield, .after = article_id_number)
 
-# let's make the data long
+# JENN'S RELIABILITY ----
 
-subfield_long <- psyc_subfield %>%
-  select(coder_name, id_number, subfield) %>%
+# let's filter for Jenn's data alone and make it long
+jenn <- psyc_subfield %>%
+  filter(coder_name == "Jenn Lee") %>%
+  select(coder_name, article_id_number, subfield) %>%
   pivot_wider(names_from = coder_name, values_from = subfield) 
 
+# filter Christina's data so it only has article's Jenn coded
+christina_jenn <- gold_subfield_long %>%
+  filter(article_id_number %in% c("1-10-2014", "12-9-2014", "5-10-2014", "5-3-2015", "7-1-2015"))
+
+christina_jenn_reliability <- merge(christina_jenn, jenn, by = "article_id_number")
+
 # let's take out the id column so R doesn't think "ID" is a rater
-subfield_clean <- subfield_long %>%
-  select("Helen Gu":"Will Osmand")
+christina_jenn_reliability <- christina_jenn_reliability %>%
+  select(-article_id_number)
 
-# let's replace the NAs with blanks (I think this was the root of the problems I was facing)
+cohen.kappa(christina_jenn_reliability)
+  # I don't know how to print this in a dataframe - do we need to?
+  # "weighted.kappa is (probability of observed matches - probability of expected matches)/(1 - probability of expected matches). Kappa just considers the matches on the main diagonal. Weighted kappa considers off diagonal elements as well."
+kappa2(christina_jenn_reliability)
 
-subfield_clean_NA <- subfield_clean %>%
-  mutate(`Helen Gu` = coalesce(`Helen Gu`, "")) %>%
-  mutate(`Georgia Saddler` = coalesce(`Georgia Saddler`, "")) %>%
-  mutate(`patrick mccraw` = coalesce(`patrick mccraw`, "")) %>%
-  mutate(`Jenn Lee` = coalesce(`Jenn Lee`, "")) %>%
-  mutate(`Will Osmand` = coalesce(`Will Osmand`, "")) 
+# Also, a little bit confused about how to write this in text format
 
-# now we can run the reliability analysis
+# HELEN'S RELIABILITY ----
 
-# not sure which kappa is the right one to use
+# let's filter for Helen's data alone and make it long
+helen <- psyc_subfield %>%
+  filter(coder_name == "Helen Gu") %>%
+  select(coder_name, article_id_number, subfield) %>%
+  pivot_wider(names_from = coder_name, values_from = subfield) %>%
+  mutate(`Helen Gu` = case_when(article_id_number == "2-7-2014" ~ "Social Psychology", article_id_number == "5-2-2015" ~ "Social Psychology",
+                                TRUE ~ as.character(`Helen Gu`))) # Helen put 'Other' for these two articles. Christina checked what she wrote in the 'Other' box and came to these conclusions.
 
-# fleiss - used for multiple categorical variables
-  # https://www.datanovia.com/en/lessons/fleiss-kappa-in-r-for-multiple-categorical-variables/#interpretation-magnitude-of-the-agreement 
+# filter Christina's data so it only has article's Helen coded
+christina_helen <- gold_subfield_long %>%
+  filter(article_id_number %in% c("2-6-2014", "2-7-2014", "22-4-2014", "4-2-2015", "5-2-2015"))
 
-kappam.fleiss(subfield_clean_NA)
+christina_helen_reliability <- merge(christina_helen, helen, by = "article_id_number")
 
-# If I'm understanding correctly, according to this website, we have fair to good agreement, above chance
+# let's take out the id column so R doesn't think "ID" is a rater
+christina_helen_reliability <- christina_helen_reliability %>%
+  select(-article_id_number)
 
-# light - average of all possible two-raters kappa
-  # https://www.datanovia.com/en/lessons/cohens-kappa-in-r-for-two-categorical-variables/#kappa-for-more-than-two-raters  
-kappam.light(subfield_clean_NA)
-  # again, fair to good agreement
+cohen.kappa(christina_helen_reliability)
+kappa2(christina_helen_reliability)
 
-# YAY this seems to work, ignore rest of code
+# PATRICK'S RELIABILITY ----
+
+# let's filter for Helen's data alone and make it long
+patrick <- psyc_subfield %>%
+  filter(coder_name == "patrick mccraw") %>%
+  select(coder_name, article_id_number, subfield) %>%
+  pivot_wider(names_from = coder_name, values_from = subfield) 
+
+# filter Christina's data so it only has article's patrick coded
+christina_patrick <- gold_subfield_long %>%
+  filter(article_id_number %in% c("10-4-2014", "15-4-2014", "18-4-2015", "6-1-2015", "9-4-2014"))
+
+christina_patrick_reliability <- merge(christina_patrick, patrick, by = "article_id_number")
+
+# let's take out the id column so R doesn't think "ID" is a rater
+christina_patrick_reliability <- christina_patrick_reliability %>%
+  select(-article_id_number)
+
+cohen.kappa(christina_patrick_reliability)
+kappa2(christina_patrick_reliability)
+
+# GEORGIA'S RELIABILITY ----
+
+# let's filter for Helen's data alone and make it long
+georgia <- psyc_subfield %>%
+  filter(coder_name == "Georgia Saddler") %>%
+  select(coder_name, article_id_number, subfield) %>%
+  pivot_wider(names_from = coder_name, values_from = subfield) %>%
+  mutate(`Georgia Saddler` = case_when(article_id_number == "10-3-2014" ~ "Social Psychology",
+                             TRUE ~ as.character(`Georgia Saddler`))) # Georgia put 'Other' for this article. Christina checked what she wrote in the 'Other' box and came to this conclusion.
+
+
+# filter Christina's data so it only has article's Georgia coded
+christina_georgia <- gold_subfield_long %>%
+  filter(article_id_number %in% c("1-3-2015", "10-3-2014", "14-8-2014", "8-5-2014", "9-9-2014"))
+
+christina_georgia_reliability <- merge(christina_georgia, georgia, by = "article_id_number")
+
+# let's take out the id column so R doesn't think "ID" is a rater
+christina_georgia_reliability <- christina_georgia_reliability %>%
+  select(-article_id_number)
+
+cohen.kappa(christina_georgia_reliability)
+kappa2(christina_georgia_reliability)
+
+# WILL'S RELIABILITY ----
+
+# let's filter for Helen's data alone and make it long
+will <- psyc_subfield %>%
+  filter(coder_name == "Will Osmand") %>%
+  select(coder_name, article_id_number, subfield) %>%
+  pivot_wider(names_from = coder_name, values_from = subfield) 
+
+# filter Christina's data so it only has article's Will coded
+christina_will <- gold_subfield_long %>%
+  filter(article_id_number %in% c("24-2-2014", "5-3-2014", "5-5-2014", "5-6-2014", "7-3-2014"))
+
+christina_will_reliability <- merge(christina_will, will, by = "article_id_number")
+
+# let's take out the id column so R doesn't think "ID" is a rater
+christina_will_reliability <- christina_will_reliability %>%
+  select(-article_id_number)
+
+cohen.kappa(christina_will_reliability)
+kappa2(christina_will_reliability)
+
+# Should I summarise these findings in some way? I guess this depends on whether we're putting this script on Git
 
 
 
 
-
-# so it looks like the number of raters is correct, but the subjects isn't
-
-# ATTEMPT 2
-
-# let's assign each subfield a number
-  # Behavioural Neuroscience = 1
-  # Developmental Psychology = 2
-  # Cognitive Neuroscience = 3
-  # Perception = 4
-  # Social Psychology = 5
-  # Cognition = 6
-  # Health Psychology = 7
-
-Christina = c(6, 5, 6, 2, 7)
-Helen = c(6, 5, 6, 2)
-Georgia = c(6, 5, 6, 2)
-Patrick = c(6, 5, 3, 2)
-Jenn = c(6, 6, 6, 2, 7)
-Will = c(6, 5)
-
-subfield_relaibility <- as.table(rbind(Christina, Helen, Georgia, Patrick, Jenn, Will))
-
-subfield_reliability <- as.data.frame("subfield_relaibility")
-
-categories <- c("Behavioural Neuroscience", "Developmental Psychology", "Cognitive Neuroscience", "Perception", "Social Psychology", "Cognition", "Health Psychology")
-
-dimnames(subfield_relaibility) <- list
-
-# ATTEMPT 3
-
-1A1 = c(0, 0, 0, 0, 0, 6, 0)
-1A2 = c(0, 0, 0, 0, 5, 1, 0)
-1A3 = c(0, 0, 1, 0, 0, 4, 0)
-1A4 = c(0, 5, 0, 0, 0, 0, 0)
-1A5 = c(0, 0, 0, 0, 0, 0, 2)
-
-subfield <- as.table(rbind(
-  c(0, 0, 0, 0, 0, 6, 0), c(0, 0, 0, 0, 5, 1, 0),
-  c(0, 0, 1, 0, 0, 4, 0), c(0, 5, 0, 0, 0, 0, 0),
-  c(0, 0, 0, 0, 0, 0, 2)))
-
-categories <- c("Behavioural Neuroscience", "Developmental Psychology", "Cognitive Neuroscience", "Perception", "Social Psychology", "Cognition", "Health Psychology")
-
-subfield <- as.data.frame(rbind(
-  c(0, 0, 0, 0, 0, 6, 0), c(0, 0, 0, 0, 5, 1, 0),
-  c(0, 0, 1, 0, 0, 4, 0), c(0, 5, 0, 0, 0, 0, 0),
-  c(0, 0, 0, 0, 0, 0, 2))) 
-
-%>%
-  rename(V1 == "BN", V2 == "Developmental", V3 == "CN", V4 == "Perception", V5 == "Social", V6 == "Cognition", V7 == "Health")
-
-kappam.fleiss(subfield)
-
-# the number of subjects is correct, but the number of raters isn't (one extra)
-
-# ATTEMPT 4
-
-subfield_coding <- as.data.frame(rbind(Christina = c(6, 5, 6, 2, 7), Helen = c(6, 5, 6, 2), Georgia = c(6, 5, 6, 2), Patrick = c(6, 5, 3, 2), Jenn = c(6, 6, 6, 2, 7), Will = c(6, 5)))
-
-# Nope, this is still the wrong format
-
-# ATTEMPT 5
-
-# let's assign each subfield a number
-# Behavioural Neuroscience = 1
-# Developmental Psychology = 2
-# Cognitive Neuroscience = 3
-# Perception = 4
-# Social Psychology = 5
-# Cognition = 6
-# Health Psychology = 7
-
-subfield_long_recoded <- subfield_long %>%
-  recode('Behavioural Neuroscience' = "1")
-  select(case_when("Helen Gu" == "Behavioural Neuroscience" ~ 1, "Helen Gu" == "Developmental Psychology" ~ 2, "Helen Gu"" == "Cognitive Neuroscience"" ~ 3, "Helen Gu" == "Perception" ~ 4, "Helen Gu"" == "Social Psychology" ~ 5, "Helen Gu" == "Cognition" ~ 6, "Helen Gu"" == "Health Psychology"))
-  
-  # trying to recode the subfields as numbers so that I can put in this format
-    # each row is a Check article
-    # each column is a coder
-  
 
 
 
