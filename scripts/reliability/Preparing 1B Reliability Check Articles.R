7ec5aa7d16cb441a93265f0c7d31b6a6f9954364
ChristinaGitHub1 <c.rochios@student.unsw.edu.au>
2021-08-11 09:43:54 +1000

    Christina reading gold standard reliability 1B data from Qualtrics

--- scripts/reliability/Preparing 1B Reliability Check Articles.R
+++ scripts/reliability/Preparing 1B Reliability Check Articles.R
@@ -6,7 +6,7 @@ library(janitor)
 
 # Import 1B data from qualtrics 
 
-qualtrics_api_credentials(api_key = "RZNCuPM7MvUJmhi7wcSLN7NHx0WCVeIU6H7XiSeE", 
+qualtrics_api_credentials(api_key = "x6zwQgKWTkduv783BnlSXfwam8yGGYLRpEcZrAnF", 
                           base_url = "unsw.syd1.qualtrics.com",
                           install = TRUE, 
                           overwrite = TRUE)
@@ -14,22 +14,16 @@ qualtrics_api_credentials(api_key = "RZNCuPM7MvUJmhi7wcSLN7NHx0WCVeIU6H7XiSeE",
 readRenviron("~/.Renviron")  
 
 # pull 1B data
-data1B  <- fetch_survey(surveyID = "SV_86Um9YiXUcH6eGy", verbose = TRUE,force_request = TRUE) %>%
+data1B  <- fetch_survey(surveyID = "SV_6nAyR8nkKxepwxw", verbose = TRUE,force_request = TRUE) %>%
   clean_names()
 
-# write 1B data to csv
-
-data1B %>% write_csv(here::here("data_files", "2021_07_22-data1B.csv"))
-
-#filter data1B to only include 'Check' articles and select just variables of interest
+#filter data1B to select only variables of interest
 data1B_select <- data1B %>%
-  filter(str_detect(q4_1, 'Check')) %>%
-  select(q2:q50)
+  select(q2, q4_1, q9:q50)
 
 # let's rename the variables
 data1B_rename <- data1B_select %>%
-  rename(coder_name = q2, coder_email = q3, no_of_experiments = q51, type_of_article = q52, type_of_article_other = q52_2_text, participants = q5, 
-         participants_other = q5_3_text, age = q6, brain_beh = q7, topic = q8, topic_other = q8_5_text, 
+  rename(coder_name = q2, 
          
          data_badge = q9, data_statement_present = q10, data_statement_indicates = q11, data_quote = q12, data_page_no = q13, 
          data_accessible = q14, data_accessible_other = q14_4_text, dataset_URL = q15, type_of_data_repo = q16, type_of_data_repo_other = q16_3_text, 
@@ -42,13 +36,10 @@ data1B_rename <- data1B_select %>%
          materials_complete = q41, materials_explanation = q42, materials_other_info = q43, supplemental_info = q49, URL_supplemental_info = q50) 
 
 # separate article id number and journal code
-data1B_sep <- data1B_rename %>%
-  separate(q4_1, into = c("type_of_article", "check_id"), sep = "\\s", remove = FALSE) %>%
-  rename(article_id_number_and_title = q4_1)
-
-# remove any duplicated rows
-data1B_clean <- data1B_sep %>%
-  distinct()
+sep1B <- data1B_rename %>%
+  separate(q4_1, into = "article_id_number", sep = "\\s", remove = FALSE) %>%
+  rename(article_id_number_and_title = q4_1) %>%
+  mutate(article_id_number = str_remove_all(article_id_number, "\""))
 
 # write a .csv file that only contains Reliability Check articles
-data1B_clean %>% write_csv(here::here("data_files", "data1B_reliability_checking"))
+sep1B %>% write_csv(here::here("data_files", "data1B_reliability_checking"))
